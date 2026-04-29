91f15aeff9e216d013f96fbf572459e9c6192684
VIVEK NANDA K <145146077+viveknandakaruppasamy@users.noreply.github.com>
2026-04-29 19:21:06 +0530

    commit
    added login functionality

--- pages/loginpage.js
+++ pages/loginpage.js
@@ -1,9 +1,10 @@
 class LoginPage {
-    constructor(page)
+    constructor(page) //for opening browser or tab
     {
         this.page = page;       
     }
 
+    // goto will open the link 
     async openPage(){
         await this.page.goto('https://www.saucedemo.com/');
     }
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import LoginPage from '../pages/loginpage';
 
-test('valid login test', async({ page }) => 
+test('valid login test', async({ page }) => //playwright work on more asynchronous elements so async keyword is used
 {
     const login = new LoginPage(page);
 
