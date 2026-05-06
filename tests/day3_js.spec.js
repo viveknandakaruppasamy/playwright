7b5b75bdc9b5e71384af2a3350f05453b4e5dc05
VIVEK NANDA K <145146077+viveknandakaruppasamy@users.noreply.github.com>
2026-05-06 21:10:42 +0530

    commit

--- tests/day3_js.spec.js
+++ tests/day3_js.spec.js
@@ -1,48 +1,47 @@
 import { test, expect } from '@playwright/test';
 
 test.use({
-    viewport: {width: 375, height: 667}
+    viewport: { width: 375, height: 667 }
 });
 
-test('UI action demo', async ({page}) =>
-{
-    //1.navigation
+test('UI action demo', async ({ page }) => {
+
+    // 1. Navigation
     await page.goto('https://www.saucedemo.com/');
 
-    //2. Get by role locator 
-    await page.getByRole('textbox',{name: 'username'}).fill('standard_user');
-    await page.getByRole('textbox', {name: 'Password'}).fill('secret_sauce');
+    // 2. Login
+    await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
+    await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce');
 
-    //3. keyboard event(Press Enter instead of clicking)
+    // 3. Keyboard event
     await page.keyboard.press('Enter');
 
-    //4. waiting for selector
+    // 4. Wait for page load
     await page.locator('.inventory_list').waitFor();
-
-    //5.expect url
-    await expect(page).toHaveURL(/inventory/);
-
-    //6. Get by text
-    await expect(page.getByText('products')).toBeVisible();
-
-    //7.clicking element
-    await page.locator('#add-to-cart-sauce-labs-backpack').click();
-
-    //8.Page assertion (cart count)
-    await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
-
-    //6.Screenshot
-    await page.screenshot({ path: 'C:/Users/viveknandha/Downloads/playwright/screenshots/inventory.png'});
-
-    await page.waitForSelector('.inventory_list');
     await page.waitForLoadState('networkidle');
 
-    await expect(page).toHaveScreenshot('inventorypage.png', {
-    maxDiffPixels: 1000
+    // 5. URL assertion
+    await expect(page).toHaveURL(/inventory/);
+
+    // 6. Text assertion
+    await expect(page.getByText('Products')).toBeVisible();
+
+    // 7. Click element
+    await page.locator('#add-to-cart-sauce-labs-backpack').click();
+
+    // 8. Cart assertion
+    await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
+
+    // 9. Normal screenshot
+    await page.screenshot({
+        path: 'screenshots/inventory.png',
+        fullPage: true
     });
 
-    // Snapshot (visual testing)
+    // 10. Visual snapshot testing
     await expect(page).toHaveScreenshot('inventorypage.png', {
-    fullPage: true
+        fullPage: true,
+        maxDiffPixels: 1000
     });
-})
+
+});
